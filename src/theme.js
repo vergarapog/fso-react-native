ae4527f7b39ce3f2a7f4d77df99e7da77e6acab7
vergarapog <vbrian.dev@gmail.com>
2025-01-15 22:39:53 +0800

    reorganize theme color

--- src/theme.js
+++ src/theme.js
@@ -2,13 +2,13 @@ import { Platform } from 'react-native';
 
 const theme = {
   colors: {
+    appBarBg: '#24292e',
+    mainBg: '#e1e4e8',
     white: '#ffffff',
     redError: '#d73a4a',
     textPrimary: '#24292e',
     textSecondary: '#586069',
     primary: '#0366d6',
-    appBarBg: '#24292e',
-    mainBg: '#e1e4e8',
   },
   fontSizes: {
     body: 14,
