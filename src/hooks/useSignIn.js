a0ebb0fda889466dc79441583ba2176f1f2c5cda
vergarapog <vbrian.dev@gmail.com>
2025-02-10 11:50:34 +0800

    enhance apollo client req, add context for auth

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,7 +1,10 @@
 import { useMutation } from '@apollo/client';
 import { LOGIN } from '../graphql/mutations';
 
+import useAuthStorage from './useAuthStorage';
+
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
   const [login, result] = useMutation(LOGIN);
 
   const signIn = async ({ username, password }) => {
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,11 +1,49 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import Constants from 'expo-constants';
+import { setContext } from '@apollo/client/link/context';
 
-const createApolloClient = () => {
+const { env } = Constants.expoConfig.extra;
+
+const httpLink = createHttpLink({
+  uri: env,
+});
+
+const createApolloClient = (authStorage) => {
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken();
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : '',
+        },
+      };
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
   return new ApolloClient({
-    uri: Constants.expoConfig.extra.env,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
+
+  // const authLink = setContext(async (_, { headers }) => {
+  //   try {
+  //     const accessToken = await authStorage.getAccessToken();
+  //     return { headers: { ...headers, authorization: accessToken ? `Bearer ${accessToken}` : '' } };
+  //   } catch (e) {
+  //     console.log(e);
+  //     return { headers };
+  //   }
+  // });
 };
+
+// const createApolloClient = () => {
+//   return new ApolloClient({
+//     uri: Constants.expoConfig.extra.env,
+//     cache: new InMemoryCache(),
+//   });
+// };
 
 export default createApolloClient;
