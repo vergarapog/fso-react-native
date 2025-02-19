b3bc017d1dc75de61144bc9afde31744b2118276
vergarapog <vbrian.dev@gmail.com>
2025-02-19 21:28:21 +0800

    add jest

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,16 @@
-const reactPlugin = require("eslint-plugin-react");
-const reactNativePlugin = require("eslint-plugin-react-native");
-const babelParser = require("@babel/eslint-parser");
+const reactPlugin = require('eslint-plugin-react');
+const reactNativePlugin = require('eslint-plugin-react-native');
+const babelParser = require('@babel/eslint-parser');
+const jestPlugin = require('eslint-plugin-jest');
 
 module.exports = [
   {
-    files: ["**/*.js", "**/*.jsx"], // Apply to JavaScript and JSX files
+    files: ['**/*.js', '**/*.jsx'], // Apply to JavaScript and JSX files
     languageOptions: {
       parser: babelParser, // Use the babel parser directly
       parserOptions: {
         ecmaVersion: 2021, // ECMAScript 2021
-        sourceType: "module", // Use ES Modules
+        sourceType: 'module', // Use ES Modules
         ecmaFeatures: {
           jsx: true, // Enable JSX parsing
         },
@@ -25,18 +26,19 @@ module.exports = [
     },
     plugins: {
       react: reactPlugin, // React plugin
-      "react-native": reactNativePlugin, // React Native plugin
+      'react-native': reactNativePlugin, // React Native plugin
+      jest: jestPlugin,
     },
     rules: {
       // React plugin recommended rules
       ...reactPlugin.configs.recommended.rules,
       // Custom rules
-      "react/prop-types": "off", // Disable prop-types rule
-      "react/react-in-jsx-scope": "off", // Disable React in JSX scope rule (for React 17+)
+      'react/prop-types': 'off', // Disable prop-types rule
+      'react/react-in-jsx-scope': 'off', // Disable React in JSX scope rule (for React 17+)
     },
     settings: {
       react: {
-        version: "detect", // Automatically detect React version
+        version: 'detect', // Automatically detect React version
       },
     },
   },
