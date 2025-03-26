5dc70b09d0c64a87262a1dd69a1d7ba11e4a0b4e
vergarapog <vbrian.dev@gmail.com>
2025-03-26 15:25:40 +0800

    navigate to home after logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,13 +1,16 @@
 import { useApolloClient } from '@apollo/client';
 import useAuthStorage from './useAuthStorage';
+import { useNavigate } from 'react-router-native';
 
 const useLogout = () => {
   const authStorage = useAuthStorage();
   const apolloClient = useApolloClient();
+  const navigate = useNavigate();
 
   const logout = async () => {
     await authStorage.removeAccessToken();
     apolloClient.resetStore();
+    navigate('/');
   };
 
   return [logout];
